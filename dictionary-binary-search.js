1d9b2acc75a4394540028d0bd005ee97908b1a75
PSC Student <psc@billkuker.com>
2022-10-20 20:36:51 -0400

    Implemented Binary Search function

--- dictionary-binary-search.js
+++ dictionary-binary-search.js
@@ -1,8 +1,34 @@
 import words from "./words.js";
 
 function binarySearch(needle, haystack){
-    //❓ Question 7, write it!
+   
+    let currentArrayEndPoint = haystack.length;
+    let currentArrayStartPoint = 0;
+    let currentArrayLength = currentArrayEndPoint - currentArrayStartPoint;
+    let middleWord = Math.round((currentArrayEndPoint + currentArrayStartPoint) / 2);
+
+    while(currentArrayLength > 1){
+    
+
+        if(needle == (haystack[middleWord])){
+            return true;
+        } else if(needle > haystack[middleWord]){
+            currentArrayStartPoint = middleWord + 1;
+            middleWord = Math.round((currentArrayEndPoint + currentArrayStartPoint) / 2);
+        } else if(needle < haystack[middleWord]){
+            currentArrayEndPoint = middleWord - 1;
+            middleWord = Math.round((currentArrayEndPoint + currentArrayStartPoint) / 2);
+        }
+    currentArrayLength = currentArrayEndPoint - currentArrayStartPoint;
+    }
+
+    if(needle == haystack[currentArrayEndPoint] || needle == haystack[currentArrayStartPoint]) {
+        return true;
+    } else {
+
     return false;
+
+    }
 }
 
 export default function(word){
